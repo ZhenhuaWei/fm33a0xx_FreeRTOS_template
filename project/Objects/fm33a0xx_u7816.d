505e644a8007ed1b468611d51e540dd10a138723
weizhenhua <weizhenhua94@163.com>
2020-09-04 10:00:55 +0800

    freertos in m0 example

---
+++ project/Objects/fm33a0xx_u7816.d
@@ -0,0 +1,11 @@
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Drivers\fm33a0xx_u7816.c
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Drivers\FM33A0XX_u7816.h
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Include\FM33A0XX.h
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Include\core_cm0plus.h
+.\objects\fm33a0xx_u7816.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Include\core_cminstr.h
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Include\core_cmfunc.h
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Include\system_FM33A0XX.h
+.\objects\fm33a0xx_u7816.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Include\FM33A0XX.h
+.\objects\fm33a0xx_u7816.o: ..\fm33a0xx\Drivers\fm33a0xx_conf.h
